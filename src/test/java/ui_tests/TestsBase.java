a9e226ac3b7cb0d1d3a3917cd5984b0c8dc096fb
Aleksei Dementev <karabass18@gmail.com>
2023-04-01 20:04:50 +0500

    added tests

--- src/test/java/ui_tests/TestsBase.java
+++ src/test/java/ui_tests/TestsBase.java
@@ -4,24 +4,27 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import webDrivers.WebDriverProvider;
 
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-//import static com.codeborne.selenide.Selenide.open;
 
 public class TestsBase {
+
+   // ContentConfig contentConfig = ConfigFactory.create(ContentConfig.class, System.getProperties());
     @BeforeAll
     static void beforeAll() {
 
-        //WebDriverProvider.configuration();
+        WebDriverProvider.configuration();
         Configuration.browserSize = "1920x1080";
-        baseUrl = "https://greetgo.ru";
+        Configuration.baseUrl = "https://greetgo.ru";
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
